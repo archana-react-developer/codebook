e4b41c25e40ece3a756c60b28eb41bfb846afce8
Archana Potnis <potnis.archana@gmail.com>
2026-04-27 18:38:58 -0700

    Update Header.js
    Updated header.js

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -29,7 +29,7 @@ export const Header = () => {
           <Link to="/" className="flex items-center">
             <img src={Logo} className="mr-3 h-10" alt="EBookify Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Digital Book E-Commerce Platform
+              E-Commerce Platform for Digital Books (Checkout & Payments)
             </span>
           </Link>
           <div className="flex items-center relative">
